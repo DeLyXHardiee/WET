d0906da204bcd02a87cb44fe45bc784c137450f0
Lukas Eliassen <luel@itu.dk>
2024-04-23 13:46:31 +0200

    measure angle

--- Processing/Analyze.py
+++ Processing/Analyze.py
@@ -1,7 +1,7 @@
 import numpy as np
 from memory_profiler import profile
 
-eyes = (0,0,55)
+eyes = np.array([0.0,3.6,55.0])
 
 def measure_saccade_accuracy(true_data, predicted_data):
     if len(true_data) != len(predicted_data):
@@ -31,31 +31,20 @@ def measure_saccade_accuracy(true_data, predicted_data):
 def measure_degrees_of_visual_angle(modifiedData, unmodifiedData):
     acc = 0
     for i in range(len(unmodifiedData)):
-        a = (unmodifiedData[i][1][0],unmodifiedData[i][1][1],0)
-        b = (modifiedData[i][2][0],modifiedData[i][2][1],0)
-        acc += angle_between_points(a,b,eyes)
+        #print(unmodifiedData[i])
+        a = np.array([unmodifiedData[i][1],unmodifiedData[i][2],0.0])
+        #print(a)
+        b = np.array([modifiedData[i][1],modifiedData[i][2],0.0])
+        acc += angle_between_points(a,eyes,b)
     return acc/len(unmodifiedData)
 
 def angle_between_points(A, B, C):
     # Calculate vectors AB and BC
-    AB = np.array(B) - np.array(A)
-    BC = np.array(C) - np.array(B)
-    
-    # Calculate dot product
-    dot_product = np.dot(AB, BC)
-    
-    # Calculate magnitudes of vectors
-    magnitude_AB = np.linalg.norm(AB)
-    magnitude_BC = np.linalg.norm(BC)
-    
-    # Calculate cosine of the angle
-    cos_angle = dot_product / (magnitude_AB * magnitude_BC)
-    
-    # Convert cosine to angle in degrees
-    angle = np.arccos(cos_angle)
-    angle_degrees = np.degrees(angle)
-    
-    return angle_degrees
+    BA = A - B
+    BC = C - B 
+    cosine_angle = np.dot(BA, BC) / (np.linalg.norm(BA) * np.linalg.norm(BC))
+    angle = np.arccos(cosine_angle)
+    return np.degrees(angle)
 
 def normalized_cross_correlation(signal1, signal2):
     # Convert signals to numpy arrays
